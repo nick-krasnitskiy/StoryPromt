d41b2246f335f91636f9cbc03f6e9df1353af83c
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-05-21 13:08:00 +0300

    IB Actions and Outlets were added.

--- StoryPromt/ViewController.swift
+++ StoryPromt/ViewController.swift
@@ -9,13 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let storyPromt = StoryPromptEntry()
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPromt.number = Int(sender.value)
+    }
+    
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPromt.genre = genre
+        } else {
+            storyPromt.genre = .scifi
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPromt = StoryPromptEntry()
+        
+        numberSlider.value = 7.5
+        
         storyPromt.noun = "toaster"
         storyPromt.adjective = "smelly"
         storyPromt.verb = "burps"
-        storyPromt.number = 10
+        storyPromt.number = Int(numberSlider.value)
         print(storyPromt)
     }
 
